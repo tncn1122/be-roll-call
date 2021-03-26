ca8066ff886f7b5d9ae4ff80fb3f708f0db4fd4b
tncn1122 <tncnhan@gmail.com>
2021-03-26 23:01:24 +0700

    fix doc

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
