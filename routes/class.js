6f42723da72a2a8e3866b32ef309dc9dd0da2eb3
tncn1122 <tncnhan@gmail.com>
2021-07-12 22:04:22 +0700

    add delete class

--- routes/class.js
+++ routes/class.js
@@ -62,5 +62,31 @@ const userUtil = require('../util/UserUtils')
     });
 })
 
+/**
+ * Xóa một lớp khỏi hệ thống dựa vào ID, chỉ có Admin mới thực hiện được chức năng này.
+ * @route DELETE /classes/{id}
+ * @group Classes
+ * @param {string} id.path.required ID của lớp cần xóa.
+ * @returns {Error.model} 200 - "Xóa thành công!" nếu thao tác thành công.
+ * @returns {Error.model} 500 - Lỗi.
+ * @security Bearer
+ */
+ router.delete('/:id', auth.isAdmin, async(req, res) => {
+    try{
+        let classId = req.params.id;
+        if (userUtil.findUser(classId)){
+            await User.deleteOne({id: classId})
+            res.status(200).send(ResponseUtil.makeMessageResponse(stringMessage.deleted_successfully))
+        }
+        else{
+            res.status(404).send(ResponseUtil.makeMessageResponse(stringMessage.user_not_found))
+        }
+        
+    }
+    catch(err){
+        log(err)
+        res.status(500).send(ResponseUtil.makeMessageResponse(error.message))
+    }
+})
 
 module.exports = router;
